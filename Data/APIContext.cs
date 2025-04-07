2cf7aea065a24de876b33d4f3b3a9d040d078db6
Artur <Artur@DESKTOP-710ML57>
2025-04-08 00:42:08 +0400

    Add project files.

---
+++ Data/APIContext.cs
@@ -0,0 +1,11 @@
+﻿using Homework1.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Homework1.Data
+{
+	public class APIContext(DbContextOptions<APIContext> options) : DbContext(options)
+	{
+		public DbSet<Post> Posts { get; set; }
+		public DbSet<User> Users { get; set; }
+	}
+}
---
+++ Program.cs
@@ -0,0 +1,41 @@
+
+using Homework1.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace Homework1
+{
+	public class Program
+	{
+		public static void Main(string[] args)
+		{
+			var builder = WebApplication.CreateBuilder(args);
+
+			// Add services to the container.
+
+			builder.Services.AddDbContext<APIContext>(option => option.UseInMemoryDatabase("PostsDB"));
+
+			builder.Services.AddControllers();
+			// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+			builder.Services.AddEndpointsApiExplorer();
+			builder.Services.AddSwaggerGen();
+
+			var app = builder.Build();
+
+			// Configure the HTTP request pipeline.
+			if (app.Environment.IsDevelopment())
+			{
+				app.UseSwagger();
+				app.UseSwaggerUI();
+			}
+
+			app.UseHttpsRedirection();
+
+			app.UseAuthorization();
+
+
+			app.MapControllers();
+
+			app.Run();
+		}
+	}
+}
